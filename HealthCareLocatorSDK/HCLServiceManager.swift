149499a990c03ee6313dbff05123f995090a8acb
Le Ngo Quang Truong <quang-truong.le-ngo@ekino.com>
2021-02-17 11:24:25 +0700

    fix(improvement): Change API key without restart the app

--- HealthCareLocatorSDK/HCLServiceManager.swift
+++ HealthCareLocatorSDK/HCLServiceManager.swift
@@ -13,7 +13,16 @@ class HCLServiceManager: NSObject {
     static let shared = HCLServiceManager()
     private var apiKey: String?
     
-    lazy private var apollo: ApolloClient = {
+    private var apollo: ApolloClient?
+    
+    private override init() {}
+    
+    func initialize(apiKey: String) {
+        self.apiKey = apiKey
+        self.apollo = initClientWith(apiKey: apiKey)
+    }
+    
+    private func initClientWith(apiKey: String) -> ApolloClient {
         // The cache is necessary to set up the store, which we're going to hand to the provider
         let cache = InMemoryNormalizedCache()
         let store = ApolloStore(cache: cache)
@@ -25,19 +34,13 @@ class HCLServiceManager: NSObject {
         
         let requestChainTransport = RequestChainNetworkTransport(interceptorProvider: provider,
                                                                  endpointURL: url,
-                                                                 additionalHeaders: ["Ocp-Apim-Subscription-Key" : apiKey.orEmpty])
+                                                                 additionalHeaders: ["Ocp-Apim-Subscription-Key" : apiKey])
         
         
         // Remember to give the store you already created to the client so it
         // doesn't create one on its own
         return ApolloClient(networkTransport: requestChainTransport,
                             store: store)
-    }()
-    
-    private override init() {}
-    
-    func initialize(apiKey: String) {
-        self.apiKey = apiKey
     }
 }
 
@@ -46,21 +49,25 @@ extension HCLServiceManager {
                            locale: String?,
                            userId: String?,
                            completionHandler: @escaping ((Activity?, Error?) -> Void)) {
-        let query = ActivityByIdQuery(userId: userId,
-                                      id: id,
-                                      locale: locale)
-        apollo.fetch(query: query) { result in
-            switch result {
-            case .success(let response):
-                if let json = response.data?.activityById?.jsonObject,
-                   let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
-                   let result = try? JSONDecoder().decode(Activity.self, from: jsonData) {
-                    completionHandler(result, nil)
+        if let apollo = self.apollo {
+            let query = ActivityByIdQuery(userId: userId,
+                                          id: id,
+                                          locale: locale)
+            apollo.fetch(query: query) { result in
+                switch result {
+                case .success(let response):
+                    if let json = response.data?.activityById?.jsonObject,
+                       let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
+                       let result = try? JSONDecoder().decode(Activity.self, from: jsonData) {
+                        completionHandler(result, nil)
+                    }
+                    break
+                case .failure(let error):
+                    completionHandler(nil, error)
                 }
-                break
-            case .failure(let error):
-                completionHandler(nil, error)
             }
+        } else {
+            completionHandler(nil, HCLError.initializeConfigureValidateFailed)
         }
     }
     
@@ -69,24 +76,28 @@ extension HCLServiceManager {
                            codeTypes: [String],
                            userId: String?,
                            completionHandler: @escaping (([Code]?, Error?) -> Void)) {
-        let query = CodesByLabelQuery(first: info.first,
-                                      offset: info.offset,
-                                      userId: userId,
-                                      criteria: criteria,
-                                      codeTypes: codeTypes,
-                                      locale: info.locale)
-        apollo.fetch(query: query) { result in
-            switch result {
-            case .success(let response):
-                if let json = response.data?.codesByLabel?.jsonObject,
-                   let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
-                   let result = try? JSONDecoder().decode(CodeResult.self, from: jsonData) {
-                    completionHandler(result.codes, nil)
+        if let apollo = self.apollo {
+            let query = CodesByLabelQuery(first: info.first,
+                                          offset: info.offset,
+                                          userId: userId,
+                                          criteria: criteria,
+                                          codeTypes: codeTypes,
+                                          locale: info.locale)
+            apollo.fetch(query: query) { result in
+                switch result {
+                case .success(let response):
+                    if let json = response.data?.codesByLabel?.jsonObject,
+                       let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
+                       let result = try? JSONDecoder().decode(CodeResult.self, from: jsonData) {
+                        completionHandler(result.codes, nil)
+                    }
+                    break
+                case .failure(let error):
+                    completionHandler(nil, error)
                 }
-                break
-            case .failure(let error):
-                completionHandler(nil, error)
             }
+        } else {
+            completionHandler(nil, HCLError.initializeConfigureValidateFailed)
         }
     }
     
@@ -95,23 +106,27 @@ extension HCLServiceManager {
                                     criteria: String!,
                                     userId: String?,
                                     completionHandler: @escaping (([IndividualWorkPlaceDetails]?, Error?) -> Void)) {
-        let query = IndividualsByNameQuery(first: info.first,
-                                           offset: info.offset,
-                                           userId: userId,
-                                           criteria: criteria,
-                                           locale: info.locale)
-        apollo.fetch(query: query) { result in
-            switch result {
-            case .success(let response):
-                if let json = response.data?.individualsByName?.jsonObject,
-                   let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
-                   let result = try? JSONDecoder().decode(IndividualResult.self, from: jsonData) {
-                    completionHandler(result.individuals, nil)
+        if let apollo = self.apollo {
+            let query = IndividualsByNameQuery(first: info.first,
+                                               offset: info.offset,
+                                               userId: userId,
+                                               criteria: criteria,
+                                               locale: info.locale)
+            apollo.fetch(query: query) { result in
+                switch result {
+                case .success(let response):
+                    if let json = response.data?.individualsByName?.jsonObject,
+                       let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
+                       let result = try? JSONDecoder().decode(IndividualResult.self, from: jsonData) {
+                        completionHandler(result.individuals, nil)
+                    }
+                    break
+                case .failure(let error):
+                    completionHandler(nil, error)
                 }
-                break
-            case .failure(let error):
-                completionHandler(nil, error)
             }
+        } else {
+            completionHandler(nil, HCLError.initializeConfigureValidateFailed)
         }
     }
     
@@ -122,27 +137,31 @@ extension HCLServiceManager {
                              criteria: String?,
                              userId: String?,
                              completionHandler: @escaping (([ActivityResult]?, Error?) -> Void)) {
-        let query = ActivitiesQuery(first: info.first,
-                                    offset: info.offset,
-                                    userId: userId,
-                                    locale: info.locale,
-                                    professionalType: nil,
-                                    specialties: specialties,
-                                    county: county,
-                                    criteria: criteria,
-                                    location: location)
-        apollo.fetch(query: query, cachePolicy: .fetchIgnoringCacheCompletely) { result in
-            switch result {
-            case .success(let response):
-                if let json = response.data?.jsonObject,
-                   let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
-                   let result = try? JSONDecoder().decode(ActivityResponse.self, from: jsonData) {
-                    completionHandler(result.activities, nil)
+        if let apollo = self.apollo {
+            let query = ActivitiesQuery(first: info.first,
+                                        offset: info.offset,
+                                        userId: userId,
+                                        locale: info.locale,
+                                        professionalType: nil,
+                                        specialties: specialties,
+                                        county: county,
+                                        criteria: criteria,
+                                        location: location)
+            apollo.fetch(query: query, cachePolicy: .fetchIgnoringCacheCompletely) { result in
+                switch result {
+                case .success(let response):
+                    if let json = response.data?.jsonObject,
+                       let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .fragmentsAllowed),
+                       let result = try? JSONDecoder().decode(ActivityResponse.self, from: jsonData) {
+                        completionHandler(result.activities, nil)
+                    }
+                    break
+                case .failure(let error):
+                    completionHandler(nil, error)
                 }
-                break
-            case .failure(let error):
-                completionHandler(nil, error)
             }
+        } else {
+            completionHandler(nil, HCLError.initializeConfigureValidateFailed)
         }
     }
 }
