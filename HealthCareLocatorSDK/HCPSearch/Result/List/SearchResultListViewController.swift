99ee0d2a7e284ad9ff6abb3b03ac7448006c4f0e
the-loi.nguyen <the-loi.nguyen@ekino.com>
2021-09-30 17:02:02 +0700

    fix(OKSDK-386): Results are wrongly highlighted in iOS.

--- HealthCareLocatorSDK/HCPSearch/Result/List/SearchResultListViewController.swift
+++ HealthCareLocatorSDK/HCPSearch/Result/List/SearchResultListViewController.swift
@@ -12,7 +12,7 @@ class SearchResultListViewController: UITableViewController, ActivityListHandler
     static let shared = SearchResultListViewController()
     weak var delegate: ActivityHandler?
     var result: [ActivityResult] = []
-    var selectedIndexs: [Int] = []
+    var selectedLocations: [Geopoint] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +27,14 @@ class SearchResultListViewController: UITableViewController, ActivityListHandler
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        if let first = SearchResultListViewController.shared.selectedIndexs.first, result.count > 0 {
-            tableView.scrollToRow(at: IndexPath(row: first, section: 0),
-                                  at: .top, animated: true)
-        }
+        guard let first = SearchResultListViewController.shared.selectedLocations.first else { return }
+        guard let index = self.result.firstIndex(where: {$0.activity.workplace.address.location == first}) else { return }
+        tableView.scrollToRow(at: IndexPath(row: index, section: 0), at: .top, animated: true)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        SearchResultListViewController.shared.selectedIndexs = []
+        SearchResultListViewController.shared.selectedLocations = []
     }
     
     func layoutWith(theme: HCLThemeConfigure) {
@@ -58,8 +57,9 @@ class SearchResultListViewController: UITableViewController, ActivityListHandler
         else {
             return .init()
         }
+        let location = result[indexPath.row].activity.workplace.address.location ?? Geopoint(lat: 0, lon: 0)
         cell.configWith(theme: theme, icons: icons, item: result[indexPath.row],
-                        selected: SearchResultListViewController.shared.selectedIndexs.contains(indexPath.row) == true)
+                        selected: SearchResultListViewController.shared.selectedLocations.contains(location))
         return cell
     }
 
